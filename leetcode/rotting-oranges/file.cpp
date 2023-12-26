891e2004ac19d9a9e834b29f36757cfb37dd98c7
2tay <hakim.toutay@e-polytechnique.ma>
2023-12-26 19:38:50 +0100

    rotting oranges

--- leetcode/rotting-oranges/file.cpp
+++ leetcode/rotting-oranges/file.cpp
@@ -1,83 +1,112 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rotten(vector<vector<int>> grid) {
-    int size = grid.size() * grid[0].size();
-    vector<vector<int>> adj;
-    vector<int> visited(size, 0);
-    int rott = -1;
-    int pos = 0;
-
-    for(int i=0; i<grid.size(); i++) {
-        for(int j=0; j<grid[i].size(); j++) {
-            vector<int> helper;
-            if(i-1>=0)
-                helper.push_back(pos-3);
-            if(j-1>=0)
-                helper.push_back(pos-1);
-            if(j+1<grid[i].size())
-                helper.push_back(pos+1);
-            if(i+1<grid.size())
-                helper.push_back(pos+3);
-            
-            if(grid[i][j] == 2)
-                rott = pos;
-            if(grid[i][j] == 0)
-                visited[pos] = 1;
-
-            adj.push_back(helper);
-            pos++;
+bool AllVisited(vector<int> visited) {
+    for(int num : visited) {
+        if(num != 1) {
+            return false;
         }
     }
 
-    if(rott == -1)
-        return -1;
+    return true;
+}
 
-    bool check = false;
-    int time = 0;
-    queue<int> q;
-    q.push(rott);
-    visited[rott] = 1;
+void pushToAdjacency(pair<int,int> index, int rows, int cols, vector<pair<int, int>> &temp) {
+    vector<pair<int, int>> ps = {{index.first-1, index.second}, {index.first+1, index.second}, {index.first, index.second-1}, {index.first, index.second+1}};
+    for(auto couple : ps) {
+        if(couple.first>=0 && couple.first<rows && couple.second>=0 && couple.second<cols) {
+            temp.push_back(couple);
+        }
+    }
+}
 
-    while(!q.empty()) {
-        int front = q.front();
-        cout << front << ": ";
+auto setAdjacency(vector<vector<int>> grid, vector<vector<pair<int, int>>> adj) {
+    int rows = grid.size();
+    
+    for(int i=0; i<rows; i++) {
+        int cols = grid[i].size();
+        for(int j=0; j<cols; j++) {
+            vector<pair<int, int>> temp;
+            temp.push_back({i, j});
+            pushToAdjacency({i,j}, rows, cols, temp);
+            adj.push_back(temp);
+        }
+    }
 
-        for(int num: adj[front]) {
-            if(visited[num] == 0) {
-                check = true;
-                visited[num] = 1;
-                q.push(num);
-                cout << num << " ";
+    return adj;
+}
+
+ int orangesRotting(vector<vector<int>>& grid) {
+    vector<vector<pair<int, int>>> adj;
+    adj = setAdjacency(grid, adj);
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    int count = 0;
+    vector<pair<int, int>> rotten;
+    vector<int> visited(rows*cols, 0);
+    
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            if(grid[i][j] == 2) {
+                rotten.push_back({i, j});
+                visited[i*rows + j] = 1;
+            }
+            if(grid[i][j] == 0) {
+                visited[i*rows + j] = 1;
+            }
+        }
+    }
+
+    if(rotten.empty()) {
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(grid[i][j] == 1) {
+                    return -1;
+                }
             }
         }
 
-        if(check) {
-            check = false;
-            time++;
+        return 0;
+    }
+
+    queue<vector<pair<int, int>>> q;
+    q.push(rotten);
+
+    while(!q.empty()) {
+        auto front = q.front();
+        vector<pair<int, int>> temp;
+
+        for(auto current : front) {
+            int index = current.first*rows + current.second;
+            for(int i=1; i<adj[index].size(); i++) {
+                auto adjacent = adj[index][i];
+                int ps = adjacent.first * rows + adjacent.second;
+                if(visited[ps] == 0) {
+                    temp.push_back(adjacent);
+                    visited[ps] = 1;
+                }
+            }
         }
 
-        cout << ", time: " << time << endl;
+        if (!temp.empty()) {
+        q.push(temp);  
+        count++;        
+    }
 
         q.pop();
     }
 
-    for(int num: visited) {
-        if(num != 1) {
-            return -1;
-        }
+    if(!AllVisited(visited)) {
+        return -1;
     }
 
-    return time;
-}
+    return count;
+} 
 
 int main() {
-    //vector<vector<int>> grid = {{2,1,1},{1,1,0},{0,1,1}};
-    //vector<vector<int>> grid ={{1},{2}};
-    vector<vector<int>> grid = {{2,1,1},{0,1,1},{1,0,1}};
-    int res = rotten(grid);
-
+    vector<vector<int>> grid = /*{{2,1,1}, {1,1,2}, {0,1,1}};*/ /*{{1}, {2}};*/ {{2,1,1}, {1,1,0}, {0,1,1}};
+    int res = orangesRotting(grid);
     cout << res;
-
     return 0;
 }
